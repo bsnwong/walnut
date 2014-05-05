064e6ae6eece8b0b1a5f11c7f539e3f24420a704
bsnwong <bsn.huang@gmail.com>
2014-05-05 14:12:23 +0800

    add css files and javascript files

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -13,13 +13,13 @@
 
                         </ul>
                     </li>
-                    <li class="nav1-li">个人
+                    <li class="nav1-li has_child">个人
                         <ul class="nav2">
                             <li class="nav2-li">成绩查询</li>
                             <li class="nav2-li">个人信息</li>
                         </ul>
                     </li>
-                    <li class="nav1-li">功能
+                    <li class="nav1-li has_child">功能
                         <ul class="nav2">
                             <li class="nav2-li">管理试题</li>
                             <li class="nav2-li">选择试题</li>
@@ -42,8 +42,12 @@
 
             </div>
         </div>
-        <div id="footer"></div>
+        <div class="clear"></div>
+        <div id="footer">
+            Powered by<a style="color: #FF6347"> Laravel </a> | Designed by <a style="color: #FF6347">Bsn</a>
+        </div>
     </div>
 </body>
+{{ HTML::script('javascript/jquery.js') }}
 {{ HTML::script('javascript/main.js') }}
 </html>
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,3 +1,33 @@
 /**
+ *
  * Created by bsn on 14-5-5.
  */
+$(document).ready(function() {
+    $(".has_child").mouseover(function() {
+        var current = $(this);
+        var children =  current.children();
+        children.each(function() {
+            if($(this).css('display') == 'none') {
+                $(this).fadeIn(200);
+            }
+        });
+    });
+    $(".has_child").mouseleave(function() {
+        var children =  $(this).children();
+        children.each(function() {
+            $(this).hide();
+        });
+    });
+    $(".nav1-li").each(function() {
+        var r = parseInt(Math.random() * 256 + 1);
+        var g = parseInt(Math.random() * 256 + 1);
+        var b = parseInt(Math.random() * 256 + 1);
+        $(this).css("border-bottom", "5px solid rgb(" + r + "," + g + "," + b + ")");
+        $(this).mouseover(function() {
+            $(this).css("border-bottom", "5px solid #000");
+        });
+        $(this).mouseleave(function() {
+            $(this).css("border-bottom", "5px solid rgb(" + r + "," + g + "," + b + ")");
+        });
+    });
+});
